9c64c9280b337201bd524297708f65ee9e66cf99
dangngocmai <39309064+dangngocmai@users.noreply.github.com>
2018-07-26 15:11:29 -0400

    Add files via upload

---
+++ about.php
@@ -0,0 +1,113 @@
+<!DOCTYPE HTML>
+<html lang = "en">
+<head>
+    
+    <meta charset = "UTF-8"/>
+  
+     <image  align = "top" src = "https://computermuseum.uwaterloo.ca/themes/uwcm/assets/pawtucket/graphics/ca_logo_main.png" width = "50%" height = "40%"> </image>
+    <image align = "top" src = "https://www.timeshighereducation.com/sites/default/files/styles/medium/public/3._logo.png?itok=NojwkvMA"  width = "10%" height = "10%"></image>
+      <!--create column to put text in with same height and width-->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {
+    box-sizing: border-box;
+}
+
+/* Create two equal columns that floats next to each other */
+.column {
+    float: left;
+    width: 50%;
+    padding: 10px;
+    height: 300px; 
+}
+
+/* Clear floats after the columns */
+.row:after {
+    content: "";
+    display: table;
+    clear: both;
+}
+
+/* Responsive layout - makes the two columns stack on top of each other instead of next to each other */
+@media screen and (max-width: 600px) {
+    .column {
+        width: 100%;
+    }
+}
+    
+    
+    .what{
+        
+        background-color: aqua;
+            color: #080000;
+    }
+</style>
+
+    </head>
+    <h1> About Computer Museum </h1> 
+    <body style="background-color: white">
+            <div class="row">
+  <div class="column" style="background-color:white;">
+    <h2 class = "what"> Who are we?</h2>
+    <p> The University of Waterloo Computing Museum is university-based museum, whose purpose is to collect, preserve and interpret the history of computing at the University of Waterloo (UW) and related organizations of the geographic region. <br> <br>
+        
+       The time period it focuses on is primarily from 1957 (the year UW was established) to the present. The museum collects various computing related artifacts: hardware, software, documents, books, manuals, and other related cultural artifacts. <br><br>
+       
+          As appropriate, it collaborates and cooperates with other UW units that hold overlapping interests, such as the University Archives and Records Management or various Alumni offices.<br><br>
+        
+          The museum has physical and online exhibits, research, teaching, public events and publications. <br>  </p>
+      
+      <h2 class = "what"> Donation </h2>
+      
+      <p>  Do you: </p>
+      <ul>
+      <li> have a collection of old hardware and software on punch cards? </li>
+      <li> scratch-build a personal computer for a research project? </li>
+      <li> work at a Waterloo spin-off on your co-op term and you still have some manuals? </li>
+      <li>  have your old slide rule or programmable calculator?</li>
+          <li> keep your old lab manuals from a computer course?</li>
+      
+      </ul>
+    <p> If you do, contact us!</p>
+    
+  </div>
+  <div class="column" style="background-color:white;">
+    <h2 class = "what">History of Computer Museum</h2>
+    <p>  Waterloo now is one of the main technical regions - "Canadian Sillicon Valley". Therefore, the development of technology is very important. In 2017, the Computer Science Department managers, Mr. Lawrence Folland  and Mr. Scott Campbell first came to the ideas making the university's very own exhibit. 
+        <br> <br> They started to spend their free-time finding materials, including old discarded computers. The museum required a lot of time to manage the software, fix the computers and catalogue their artifacts. The old equipment went to central stores, and was later treated as e-waste. If it had value, they sold it right away.  <br><br>
+                
+ The museum has parts from the first mainframe on campus, dating all the way back to the mid-80s. There is a slide roll and a mechanical calculator from the 19th century, and even a WWII bombsight device. They are all displayed in Davis Centre.  <br></p>
+      
+      
+      <!--contact us-->
+        <h2 class = "what"> Contact Us </h2>
+      
+  <i>  <a href="mailto:lawrence.folland@uwaterloo.ca"> Lawrence Folland</a></i>  <br>
+ School of Computer Science <br>
+Davis Centre room 2607 <br>
+519-888-4567 x32214 
+      <br><br>
+   <i> <a href="mailto:scott.campbell@uwaterloo.ca">Scott Campbell</a></i>  <br>
+ Centre for Society, Technology and Values <br>
+Engineering 3 room 3174 <br>
+    519-888-4567 x35635 
+      <br><br>
+      <i> <a href="mailto:musetech@uwaterloo.ca"> Museum Assistants </a> </i>
+ <br>
+                 </div>
+      
+
+</div>
+         <!--back to conversion-->
+     <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br><br><br><br><br><br><br><br><br><br><br><br>
+        
+       <?php echo "<a href = 'computermuseum.php' target='_blank'>Back to conversion</a>"; ?>
+    </body>
+    
+      <footer>
+          
+    <!--copy right-->
+        Website HTML &copy 2018 
+            
+        
+        </footer>
